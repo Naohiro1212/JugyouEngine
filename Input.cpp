3833c3371ebf7cef678fe44c3df758e0be288dab
Naohiro1212 <nao1212hiro@icloud.com>
2025-10-07 10:39:34 +0900

    プロジェクト ファイルを追加します。

---
+++ Input.cpp
@@ -0,0 +1,124 @@
+#include "Input.h"
+#include <DirectXMath.h>
+
+using namespace DirectX;
+
+namespace Input
+{
+	// キーボードインプット
+	LPDIRECTINPUT8 pDInput = nullptr;
+	LPDIRECTINPUTDEVICE8 pKeyDevice = nullptr;
+	BYTE keyState[256] = { 0 };
+	BYTE prevKeyState[256] = { 0 };
+
+	// マウスインプット
+	LPDIRECTINPUTDEVICE8 pMouseDevice = nullptr;
+	DIMOUSESTATE mouseState; // マウスの状態
+	DIMOUSESTATE prevMouseState; // 前回のマウスの状態
+	XMVECTOR mouseposition;
+
+	void Initialize(HWND hWnd)
+	{
+		// キーボードデバイス初期化
+		DirectInput8Create(GetModuleHandle(nullptr),
+			DIRECTINPUT_VERSION,
+			IID_IDirectInput8, (VOID**)&pDInput, nullptr);
+		pDInput->CreateDevice(GUID_SysKeyboard, &pKeyDevice, nullptr);
+		pKeyDevice->SetDataFormat(&c_dfDIKeyboard);
+		pKeyDevice->SetCooperativeLevel(hWnd, DISCL_NONEXCLUSIVE | DISCL_BACKGROUND);
+
+		// マウスデバイス初期化
+		pDInput->CreateDevice(GUID_SysMouse, &pMouseDevice, nullptr);
+		pMouseDevice->SetDataFormat(&c_dfDIMouse);
+		pMouseDevice->SetCooperativeLevel(hWnd, DISCL_NONEXCLUSIVE | DISCL_BACKGROUND);
+	}
+
+	void Update()
+	{
+		memcpy(prevKeyState, keyState, sizeof(keyState));
+		for (auto i = 0; i < 256; i++)
+		{
+			prevKeyState[i] = keyState[i];
+		}
+		pKeyDevice->Acquire();
+		pKeyDevice->GetDeviceState(sizeof(keyState), &keyState);
+
+		// マウスの状態を保存
+		pMouseDevice->Acquire();
+		memcpy(&prevMouseState, &mouseState, sizeof(mouseState));
+		pMouseDevice->GetDeviceState(sizeof(mouseState), &mouseState);
+	}
+
+	bool IsKey(int keyCode)
+	{
+		if (keyState[keyCode] & 0x80)
+		{
+			return true;
+		}
+		return false;
+
+	}
+
+	bool IsKeyUp(int keyCode)
+	{
+		// 今は押してなくて、前回は押してた
+		if (!(keyState[keyCode] & 0x80) && (prevKeyState[keyCode] & 0x80))
+		{
+			return true;
+		}
+		return false;
+	}
+
+	bool IsKeyDown(int keyCode)
+	{
+		// 今は押してて、前回は押してない
+		if ((keyState[keyCode] & 0x80) && !(prevKeyState[keyCode] & 0x80))
+		{
+			return true;
+		}
+		return false;
+	}
+
+	XMVECTOR GetMousePosition()
+	{
+		return XMVECTOR();
+	}
+
+	void SetMousePosition(int x, int y)
+	{
+		mouseposition = { (float)x, (float)y, 0.0f, 1.0f };
+	}
+
+	bool IsMouseButton(int btnCode)
+	{
+		if (mouseState.rgbButtons[btnCode] & 0x80)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	bool IsMouseButtonUp(int btnCode)
+	{
+		if (!(mouseState.rgbButtons[btnCode] & 0x80) && (prevMouseState.rgbButtons[btnCode] & 0x80))
+		{
+			return true;
+		}
+		return false;
+	}
+
+	bool IsMouseButtonDown(int btnCode)
+	{
+		if ((mouseState.rgbButtons[btnCode] & 0x80) && !(prevMouseState.rgbButtons[btnCode] & 0x80))
+		{
+			return true;
+		}
+		return false;
+	}
+
+	void Release()
+	{
+		SAFE_RELEASE(pDInput);
+	}
+
+}
---
+++ Input.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <dinput.h>
+#include <DirectXMath.h>
+
+using namespace DirectX;
+
+#pragma comment(lib, "dinput8.lib")
+#pragma comment(lib, "dxguid.lib")
+
+#define SAFE_RELEASE(p) if(p != nullptr){p->Release(); p = nullptr; }
+
+namespace Input
+{
+	void Initialize(HWND hWnd);
+	void Update();
+	bool IsKey(int keyCode);
+	bool IsKeyUp(int keyCode);
+	bool IsKeyDown(int keyCode);
+
+	XMVECTOR GetMousePosition();
+	void SetMousePosition(int x, int y);
+	bool IsMouseButton(int btnCode);
+	bool IsMouseButtonUp(int btnCode);
+	bool IsMouseButtonDown(int btnCode);
+
+	void Release();
+}
